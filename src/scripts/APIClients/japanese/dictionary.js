2bdcf57953d54d7585a9c8f3facad6a1b09359a4
W01fw00d <gabrm_91@hotmail.com>
2019-07-17 19:00:54 +0200

    Improve css styling of phrases. Add some new words

--- src/scripts/APIClients/japanese/dictionary.js
+++ src/scripts/APIClients/japanese/dictionary.js
@@ -1,6 +1,11 @@
 class Dictionary {
   get() {
     return {
+      articles: [
+        this.translation('kare', 'he', '彼'),
+        this.translation('kanojo', 'she', '彼女'),
+        this.translation('watashi', 'I', '私'),
+      ],
       adverbs: {
         time: [
           this.translation('konban', 'tonight', '今晩'),
@@ -20,13 +25,23 @@ class Dictionary {
       interjections: [
         //kara is therefore if it's on the phrase middle, and because if it's at the end
         this.translation('kara', 'therefore/because'),
-        this.translation('soshte', 'and'),
+        this.translation('soshite', 'and', 'そして'),
         this.translation('ga', 'but'),
+        this.translation('to', 'with', 'と'),
+        //'wa' is used to remark the contrast between two phrases united by a 'ga'
+        this.translation('wa', '(remarked)'),
       ],
       pronouns: {
         interrogative: [
           this.translation('nani', 'what', '何'),
           this.translation('tore', 'which one', 'どれ'),
+          this.translation('doko', 'where', 'どこ'),
+          this.translation('dōshite', 'why', 'どうして'),
+        ],
+        demonstrative: [
+          this.translation('kore', 'this', 'これ'),
+          this.translation('sore', 'that', 'それ'),
+          this.translation('āre', 'that over there', 'アーレ'),
         ]
       },
       expressions: [
@@ -36,17 +51,32 @@ class Dictionary {
       adjectives: [
         this.translation('omoshiroi', 'interesting', '面白い'),
         this.translation('oishī', 'delicious', 'おいしい'),
+        this.translation('kawaii', 'cute', '可愛い'),
+        this.translation('kowai', 'scary', '怖い'),
+
+        this.translation('nemui', 'sleepy', '眠い'),
       ],
 
       nouns: [
         this.translation('kamera', 'camera'),
         this.translation('dejikame', 'digital camera', 'デジカメ'),
         this.translation('terebi', 'television', 'テレビ'),
-        this.translation('eiga', 'movie', '映画'),
-        this.translation('Konsāto', 'concert', 'コンサート'),
+        this.translation('denwa', 'phone', '電話'),
         this.translation('tīshatsu', 't-shirt'),
 
+        this.translation('eiga', 'movie', '映画'),
+        this.translation('Konsāto', 'concert', 'コンサート'),
+
+        this.translation('gorufu', 'golf', 'ゴルフ'),
+        this.translation('tenisu', 'tennis', 'テニス'),
+
+        this.translation('shi-goto', 'work', 'しごと'),
+        this.translation('rirakkusu', 'relax', 'リラックス'),
+
+        this.translation('nihon', 'japan', '日本'),
+
         this.translation('hoteru', 'hotel'),
+        this.translation('depāto', 'department store', 'デパート'),
 
         this.translation('sandwich', 'sandoitchi', 'サンドイッチ'),
         this.translation('aisukurīmu', 'ice-cream'),
@@ -67,6 +97,8 @@ class Dictionary {
         this.translation('mi', 'see/watch/look'),
         this.translation('jomi', 'read'),
         this.translation('kai', 'buy'),
+        this.translation('shi', 'do/play'),
+        this.translation('iki', 'go to'),
       ],
       // Japanese phrases always ends with a verb, so we add the . when needed here
       verbs_particles: [
@@ -76,6 +108,9 @@ class Dictionary {
         this.particles('mas ka?', 'question', '?', 'sufix'),
         this.particles('masen ka?', 'formal proposal', ', would you?', 'sufix'),
         this.particles('masho ka?', 'proposal', ', shall we?', 'sufix'),
+        this.particles('tai des', 'desire', ', I want to ', 'prefix'),
+        //'tai des ka?' affects beginning and end of english translated phrase
+        this.particles('tai des ka?', 'desire question', ', Do you want to ', 'prefix'),
       ],
       special_verbs: [
         this.translation('sukidesu', 'like (it)'),
